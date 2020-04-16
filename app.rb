7a4f95b192f1522ebfae55c4d91b03407f33aaaf
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-16 18:35:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,9 +23,17 @@ class App < Sinatra::Base
   
   get "/:operation/:number1/:number2" do 
     @operation = params[:operation]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     case @operation
     when "add"
-      
+      (@number1 + @number2).to_i 
+    when "subtract"
+      (@number1 - @number2).to_i
+    when "multiply"
+      (@number1 * @number2).to_i
+    when "divide"
+      (@number1 / @number2).to_i
+    end 
+  end 
 end
