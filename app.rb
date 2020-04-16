ddafe215cd09a6709971cc1bfd5fb083edca605c
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-16 18:14:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-
+  
+  get "/reversename/:name" do
+    @user_name = params[:name].reverse
+  end
+  
+  get "/square/:number" do
+    @num = params[:number].to_i
+    "#{@num ** 2}.to_s"  
+  end
+  
+  get "/say/:number/:phrase" do 
+    
+    @phrase = @params[:phrase] * params[:number]
+  end 
 end
